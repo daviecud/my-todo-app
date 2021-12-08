02dfe4738c98bebcc8f10090e6b7146be0e3ffc9
David Calderwood <44207468+daviecud@users.noreply.github.com>
2021-12-08 12:59:55 +0000

    removed unused import from index.js

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { HashRouter as Router } from "react-router-dom";
 //component file
-import TodoContainer from "./functionBased/components/TodoContainer"
+//import TodoContainer from "./functionBased/components/TodoContainer"
+
 
 //stylesheet
 import "./functionBased/App.css"
